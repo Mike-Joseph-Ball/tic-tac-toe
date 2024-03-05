824821601821afcda8e3d3f18ec4d4a8e6df9153
ColdShaker <michaelball357@gmail.com>
2024-03-04 21:08:27 -0800

    commit for the night

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,29 @@
-export default function Square() {
-  return <button className="Square">X</button>
+import { useState } from 'react'
+
+function Square() {
+  const [value, setValue] = useState< null | string>(null); // this end bit is called type argument
+  function handleClick() {
+    setValue('X');
+  }
+  return <button className="square" onClick={handleClick}>{value}</button>
+}
+
+export default function Board() {
+  return <>
+  <div className = "board-row">
+  <Square />
+  <Square />
+  <Square />
+  </div>
+  <div className = "board-row">
+  <Square />
+  <Square />
+  <Square />
+  </div>
+  <div className = "board-row">
+  <Square />
+  <Square />
+  <Square />
+  </div>
+  </>
 }
